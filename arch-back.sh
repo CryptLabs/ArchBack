1e336515a19572e482263553f2c927d37398769d
Al1nuX <cryptlabs@gmail.com>
2024-11-08 14:12:08 +0300

    Fixed code.

--- arch-back.sh
+++ arch-back.sh
@@ -15,51 +15,51 @@ echo -e "${RED}A${GREEN}r${YELLOW}c${BLUE}h${MAGENTA}i${CYAN}v${RED}e${RESET}"
 
 # Function for task 1
 function task1() {
-  echo "Backing up packages from the official repository ..."
-  pacman -Qqen >pkglist-repo.txt
+	echo "Backing up packages from the official repository ..."
+	pacman -Qqen >pkglist-repo.txt
 }
 
 # Function for task 2
 function task2() {
-  echo "Backing up packages from the AUR ..."
-  pacman -Qqem >pkglist-aur.txt
+	echo "Backing up packages from the AUR ..."
+	pacman -Qqem >pkglist-aur.txt
 }
 function task3() {
-  echo "Restoring packages from the official repository ..."
-  pacman -Qqem >pkglist-aur.txt
+	echo "Restoring packages from the official repository ..."
+	pacman -Qqem >pkglist-aur.txt
 }
 function task4() {
-  echo "Restoring packages from the AUR ..."
-  pacman -Qqem >pkglist-aur.txt
+	echo "Restoring packages from the AUR ..."
+	pacman -Qqem >pkglist-aur.txt
 }
 
 # Function to display menu
 function show_menu() {
-  echo "1) Backup the official repository packages"
-  echo "2) Backup the AUR packages"
-  echo "3) Restore packages from the official repository"
-  echo "4) Restore packages from the AUR"
+	echo "1) Backup the official repository packages"
+	echo "2) Backup the AUR packages"
+	echo "3) Restore packages from the official repository"
+	echo "4) Restore packages from the AUR"
 
-  echo "Q) Quit"
+	echo "Q) Quit"
 }
 
 # Main execution
 while true; do
-  show_menu
-  read -p "Enter your choice: " CHOICE
-  case $CHOICE in
-  1)
-    task1
-    ;;
-  2)
-    task2
-    ;;
-  q)
-    echo "Exiting..."
-    break
-    ;;
-  *)
-    echo "Invalid choice, please try again."
-    ;;
-  esac
+	show_menu
+	read -r -p "Enter your choice: " CHOICE
+	case $CHOICE in
+	1)
+		task1
+		;;
+	2)
+		task2
+		;;
+	q)
+		echo "Exiting..."
+		break
+		;;
+	*)
+		echo "Invalid choice, please try again."
+		;;
+	esac
 done
